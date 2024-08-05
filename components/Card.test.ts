50c7bcec674dcdc7d0f9a27d3ae4b85783e7a93b
진민 <insomnis2020@naver.com>
2024-08-05 22:59:18 +0900

    Nuxt MSW 셋팅

--- components/Card.test.ts
+++ components/Card.test.ts
@@ -2,10 +2,15 @@ import { it, expect } from 'vitest'
 // ---cut---
 // tests/components/SomeComponents.nuxt.spec.ts
 import { mountSuspended } from '@nuxt/test-utils/runtime'
+import { setup } from "@nuxt/test-utils";
 import Card from './Card.vue'
 
 // tests/App.nuxt.spec.ts
 it('can also mount an app', async () => {
+    await setup({
+        setupTimeout: 10000,
+    });
+
     const component = await mountSuspended(Card)
     console.log(component.html())
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
-import path from 'path';
+import path from "path";
+
+const apiBaseUrl = process.env.API_URL;
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   runtimeConfig: {
     public: {
-      apiBase: 'https://api.example.com'
-    }
+      apiBase: "https://api.example.com",
+    },
   },
   build: {
     // 잘못된 설정 예시
@@ -13,11 +15,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   typescript: {
-    typeCheck: true
+    typeCheck: true,
   },
-  css: [
-    '~/assets/css/tailwind.css',
-  ],
+  css: ["~/assets/css/tailwind.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -25,34 +25,53 @@ export default defineNuxtConfig({
     },
   },
   tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
-    configPath: 'tailwind.config',
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config",
     exposeConfig: {
-      level: 2
+      level: 2,
     },
     config: {},
     viewer: true,
   },
 
   imports: {
-    autoImport: true
+    autoImport: true,
   },
   components: [
     {
-      path: '~/components',
+      path: "~/components",
       pathPrefix: false,
     },
   ],
   primevue: {
     options: {
-      unstyled: false
+      unstyled: false,
     },
-    importPT: { from: path.resolve(__dirname, './presets/aura/') },      //import and apply preset
-
+    importPT: { from: path.resolve(__dirname, "./presets/aura/") }, //import and apply preset
   },
   pinia: {
-    storesDirs: ['./stores/**'],
+    storesDirs: ["./stores/**"],
   },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-primevue", '@nuxt/test-utils/module'],
-  
-})
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "nuxt-primevue",
+    "@nuxt/test-utils/module",
+    "@nuxt/content",
+    "@nuxt/image",
+  ],
+
+  routeRules: {
+    "/api/**": {
+      proxy: `${apiBaseUrl}/api/**`,
+      cors: true,
+    },
+  },
+  content: {
+    api: {
+      baseURL: "/_content", // queryContent 함수응답용 API base URL 변경(default: '/api/_content')
+    },
+    watch: false, // websocket 기능 끄기
+  },
+});
